71d0de5ef9061509a3b07f7a64548b9ad6f1f48c
Gotenks3 <riku.murakami333@gmail.com>
2022-12-07 16:45:21 +0900

    Stock作成

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -43,4 +43,9 @@ class Product extends Model
     {
         return $this->belongsTo(Image::class, 'image1', 'id');
     }
+
+    public function stock()
+    {
+        return $this->hasMany(Stock::class);
+    }
 }
